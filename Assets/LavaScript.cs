66445d161cc40801d86851819c62c03d94fbe50c
Alan Heng <alanheng@cpp.edu>
2025-11-12 22:21:21 -0800

    Pushing of the Lava Asset
    This still needs to be updated when the players are in the program

---
+++ Assets/LavaScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class LavaScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //Update this so that you 
+    // private void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     if (collision.GetComponent<ConeHead>() != null)
+    //     {
+    //         collision.GetComponent<ConeHead>().Die();
+    //     }
+    //     else if (collision.GetComponent<Footballer> != null)
+    //         collision.GetComponent<Footballer>().Die();
+    // 
+    // }
+}
