aebe5fa35b19e7c9c577a53f824763fa9e25f8ca
Alan Heng <alanheng@cpp.edu>
2025-11-15 16:39:10 -0800

    Moving Platform
    This is a work in progress, but this does move

---
+++ Assets/MovingPlatScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class MovingPlatScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+     private float dirX;
+    private float moveSpeed = 3f;
+    private Vector3 localScale;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        localScale = transform.localScale;
+        rb = GetComponent<Rigidbody2D>();
+        dirX = -1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    // Update this so that you change the collision to the name of the wall script that we collide to 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Lava>() != null ||
+        collision.GetComponentInParent<Lava>() != null)
+        {
+            dirX *= -1f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(dirX * moveSpeed, rb.linearVelocity.y);
+    }
+}
